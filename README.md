5b330d7f109116675054548a96efd706b98bc7d1
Manolo Carrasco <manolo@vaadin.com>
2022-06-30 13:42:14 +0200

    add --list argument for split in CI containers

--- README.md
+++ README.md
@@ -9,30 +9,31 @@
 ```
 Use: ./scripts/pit/run.sh [version=] [starters=] [port=] [timeout=] [verbose] [offline] [interactive] [skiptests] [pnpm] [vite] [help]
 
-  version      Vaadin version to test, by default current stable, otherwise it runs tests against current stable and then against given version.
-  starters     List of demos o presets separated by comma to run (default: all) valid options:
-                 latest-java
-                 latest-java-top
-                 latest-javahtml
-                 latest-typescript
-                 latest-typescript-top
-                 latest-java_partial-auth
-                 latest-typescript_partial-auth
-                 skeleton-starter-flow-cdi
-                 base-starter-spring-gradle
-                 base-starter-flow-quarkus
-                 skeleton-starter-flow-spring
-                 vaadin-flow-karaf-example
-                 base-starter-flow-osgi
-  port         HTTP Port for thee servlet container (default: 8080)
-  timeout      Time in secs to wait for server to start (default 300)
-  verbose      Show server output (default silent)
-  offline      Do not remove previous folders, and do not use network for mvn (default online)
-  interactive  Play Bell and ask user to manually test the application (default non interactive)
-  skiptests    Skip Selenium IDE Tests (default run tests). Note: selenium-ide does not work in gitpod
-  pnpm         Use pnpm instead of npm to speed up frontend compilation (default npm)
-  vite         Use vite inetad of webpack to speed up frontend compilation (default webpack)
-  help         Show this message
+ --version=string  Vaadin version to test, if not given it only tests current stable, otherwise it runs tests against current stable and then against given version.
+ --starters=list   List of demos or presets separated by comma to run (default: all) valid options:
+                   · latest-java
+                   · latest-java-top
+                   · latest-javahtml
+                   · latest-typescript
+                   · latest-typescript-top
+                   · latest-java_partial-auth
+                   · latest-typescript_partial-auth
+                   · skeleton-starter-flow-cdi
+                   · base-starter-spring-gradle
+                   · base-starter-flow-quarkus
+                   · skeleton-starter-flow-spring
+                   · vaadin-flow-karaf-example
+                   · base-starter-flow-osgi
+ --port=number     HTTP port for thee servlet container (default: 8080)
+ --timeout=number  Time in secs to wait for server to start (default 300)
+ --verbose         Show server output (default silent)
+ --offline         Do not remove already downloaded projects, and do not use network for mvn (default online)
+ --interactive     Play a bell and ask user to manually test the application (default non interactive)
+ --skiptests       Skip UI Tests (default run tests). Note: selenium-ide does not work in gitpod
+ --pnpm            Use pnpm instead of npm to speed up frontend compilation (default npm)
+ --vite            Use vite inetad of webpack to speed up frontend compilation (default webpack)
+ --list            Show the list of available starters
+ --help            Show this message
 ```
 
 
--- scripts/pit/lib/lib-args.sh
+++ scripts/pit/lib/lib-args.sh
@@ -2,18 +2,18 @@ usage() {
   cat <<EOF
 Use: $0 [version=] [starters=] [port=] [timeout=] [verbose] [offline] [interactive] [skiptests] [pnpm] [vite] [help]
 
-  version      Vaadin version to test, by default current stable, otherwise it runs tests against current stable and then against given version.
-  starters     List of demos o presets separated by comma to run (default: all) valid options:
-                 `echo $DEFAULT_STARTERS | sed -e 's/,/\n                 /g'`
-  port         HTTP Port for thee servlet container (default: $DEFAULT_PORT)
-  timeout      Time in secs to wait for server to start (default $DEFAULT_TIMEOUT)
-  verbose      Show server output (default silent)
-  offline      Do not remove previous folders, and do not use network for mvn (default online)
-  interactive  Play Bell and ask user to manually test the application (default non interactive)
-  skiptests    Skip Selenium IDE Tests (default run tests). Note: selenium-ide does not work in gitpod
-  pnpm         Use pnpm instead of npm to speed up frontend compilation (default npm)
-  vite         Use vite inetad of webpack to speed up frontend compilation (default webpack)
-  help         Show this message
+ --version=string  Vaadin version to test, if not given it only tests current stable, otherwise it runs tests against current stable and then against given version.
+ --starters=list   List of demos or presets separated by comma to run (default: all) valid options:`echo ,$DEFAULT_STARTERS | sed -e 's/,/\n                   · /g'`
+ --port=number     HTTP port for thee servlet container (default: $DEFAULT_PORT)
+ --timeout=number  Time in secs to wait for server to start (default $DEFAULT_TIMEOUT)
+ --verbose         Show server output (default silent)
+ --offline         Do not remove already downloaded projects, and do not use network for mvn (default online)
+ --interactive     Play a bell and ask user to manually test the application (default non interactive)
+ --skiptests       Skip UI Tests (default run tests). Note: selenium-ide does not work in gitpod
+ --pnpm            Use pnpm instead of npm to speed up frontend compilation (default npm)
+ --vite            Use vite inetad of webpack to speed up frontend compilation (default webpack)
+ --list            Show the list of available starters
+ --help            Show this message
 EOF
   exit 1
 }
@@ -23,18 +23,19 @@ checkArgs() {
   while [ -n "$1" ]
   do
     arg=`echo "$1" | cut -d= -f2`
-    case `echo $1 | sed -e 's/\-//g'` in
-      port=*) PORT="$arg";;
-      start*=*) STARTERS="$arg";;
-      version=*) VERSION="$arg";;
-      timeout=*) TIMEOUT="$arg";;
-      verbose|debug) VERBOSE=true;;
-      offline) OFFLINE=true;;
-      interactive) INTERACTIVE=true;;
-      skip*) SKIPTESTS=true;;
-      pnpm) PNPM=true;;
-      vite) VITE=true;;
-      help|h) usage && exit 0;;
+    case "$1" in
+      --port=*) PORT="$arg";;
+      --start*=*) STARTERS="$arg";;
+      --version=*) VERSION="$arg";;
+      --timeout=*) TIMEOUT="$arg";;
+      --verbose|--debug) VERBOSE=true;;
+      --offline) OFFLINE=true;;
+      --interactive) INTERACTIVE=true;;
+      --skip*) SKIPTESTS=true;;
+      --pnpm) PNPM=true;;
+      --vite) VITE=true;;
+      --list) echo "$DEFAULT_STARTERS" | tr "," "\n" && exit 0;;
+      --help) usage && exit 0;;
       *) echo "Unknown option: $1" && usage && exit 1;;
     esac
     shift
