96a34d255b3769d42e724d4246d9a3c83723c6c3
Manolo Carrasco <manolo@vaadin.com>
2024-03-01 12:18:42 +0100

    fix old version

--- scripts/pit/lib/lib-patch.sh
+++ scripts/pit/lib/lib-patch.sh
@@ -60,7 +60,7 @@ applyPatches() {
 
   if [ "$type_" = 'current' ]; then
     case $vers_ in
-      24.3.*|2.4.*|2.5.*|3.0.*) : ;;
+      24.4.*|24.3.*|2.4.*|2.5.*|3.0.*) : ;;      
       *) reportError "Using old version $vers_" "Please upgrade $app_ to latest stable" ;;
     esac
   fi
