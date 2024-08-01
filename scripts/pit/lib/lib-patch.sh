d46c07f3eeac8d813d6ec722b7df5e717ee67101
Manolo Carrasco <manolo@vaadin.com>
2024-08-01 22:13:14 +0200

    fix ce-demo license for 24.5

--- scripts/pit/lib/lib-patch.sh
+++ scripts/pit/lib/lib-patch.sh
@@ -33,12 +33,11 @@ applyPatches() {
       rm -rf ~/vaadin/node*
       ;;
     ce-demo)
-      LIC=ce-license.json
-      [ -n "$TEST" ] && ([ -z "$CE_LICENSE" ] && cmd "## Put a valid CE License in ./$LIC" || cmd "## Copy your CE License to ./$LIC") && return 0
-      [ -z "$CE_LICENSE" ] && err "No \$CE_LICENSE provided" && [ -z "$TEST" ] && return 1
-      warn "Creating license file ./$LIC with the \$CE_LICENSE content"
-      cmd "echo \"\$CE_LICENSE\" > $LIC"
-      echo "$CE_LICENSE" > $LIC
+    echo $vers_
+      case $vers_ in
+          24.5*) patchCeLicense ;;
+          *) installCeLicense ;;
+      esac
       ;;
     form-filler-demo)
       [ -n "$TEST" ] && ([ -z "$OPENAI_TOKEN" ] && cmd "export OPENAI_TOKEN=your_AI_token") && return 0
@@ -164,3 +163,66 @@ patchTsConfig() {
   H=`ls -1 tsconfig.json */tsconfig.json 2>/dev/null`
   [ -n "$H" ] && warn "patch 23.3.0.alpha3 - Removing $H" && rm -f tsconfig.json */tsconfig.json
 }
+
+
+installCeLicense() {
+  LIC=ce-license.json
+  [ -n "$TEST" ] && ([ -z "$CE_LICENSE" ] && cmd "## Put a valid CE License in ./$LIC" || cmd "## Copy your CE License to ./$LIC") && return 0
+  [ -z "$CE_LICENSE" ] && err "No \$CE_LICENSE provided" && [ -z "$TEST" ] && return 1
+  warn "Creating license file ./$LIC with the \$CE_LICENSE content"
+  cmd "echo \"\$CE_LICENSE\" > $LIC"
+  echo "$CE_LICENSE" > $LIC
+}
+
+patchCeLicense() {
+warn "Patching CE License in src/main/java/com/jensjansson/ce/Application.java"
+cat <<EOF > src/main/java/com/jensjansson/ce/Application.java || return 1
+package com.jensjansson.ce;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.jensjansson.ce.bot.BotManager;
+import com.jensjansson.ce.data.service.PersonService;
+import com.vaadin.collaborationengine.CollaborationEngine;
+import com.vaadin.collaborationengine.CollaborationEngineConfiguration;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.VaadinServiceInitListener;
+import com.vaadin.flow.theme.Theme;
+
+/**
+ * The entry point of the Spring Boot application.
+ */
+@SpringBootApplication
+@PWA(name = "CE Demo", shortName = "CE Demo")
+@Push
+@Theme("ce-demo")
+public class Application extends SpringBootServletInitializer
+        implements AppShellConfigurator, VaadinServiceInitListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    PersonService personService;
+
+    @Override
+    public void serviceInit(ServiceInitEvent serviceInitEvent) {
+        CollaborationEngine ce = CollaborationEngine.configure(serviceInitEvent.getSource(),
+                new CollaborationEngineConfiguration());
+        BotManager.createInstance(personService, ce);
+    }
+}
+EOF
+}
+
