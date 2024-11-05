e27d7070a1e2e2e0bff26bf85aa3ba3498b96311
maximerauturier <maxime@vaadin.com>
2024-11-05 11:17:36 +0200

    Passing the passkey as an environment variable

--- scripts/pit/its/cc-setup.js
+++ scripts/pit/its/cc-setup.js
@@ -1,7 +1,7 @@
 const { chromium } = require('playwright');
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-let headless = false, host = 'localhost', port = '8000', hub = false, passKey='';
+let headless = false, host = 'localhost', port = '8000', hub = false;
 process.argv.forEach(a => {
   if (/^--headless/.test(a)) {
     headless = true;
@@ -9,8 +9,6 @@ process.argv.forEach(a => {
     ip = a.split('=')[1];
   } else if (/^--port=/.test(a)) {
     port = a.split('=')[1];
-  } else if (/^--passKey=/.test(a)) {
-    passKey = a.split('=')[1];
   }
 });
 
@@ -20,6 +18,7 @@ process.argv.forEach(a => {
     chromiumSandbox: false
   });
   const context = await browser.newContext();
+  const log = s => process.stderr.write(`   ${s}`);
 
   // Open new page
   const page = await context.newPage();
@@ -29,25 +28,26 @@ process.argv.forEach(a => {
   // Go to http://${host}:${port}/
   await page.goto(`http://${host}:${port}/`);
 
-  // Enter the passKey
-  await page.getByLabel('Passkey').fill(passKey)
-  await page.getByRole('button', { name: 'Next' }).click()
+  if(process.env.PASS_KEY) {
+    // Enter the passKey
+    await page.getByLabel('Passkey').fill(process.env.PASS_KEY)
+    await page.getByRole('button', {name: 'Next'}).click()
 
-  await page.locator('input[type="text"]').click({timeout:60000});
-  await page.locator('input[type="text"]').fill('keycloak-local.alcala.org');
-  await page.getByRole('button', { name: 'Next' }).click()
+    await page.locator('input[type="text"]').click({timeout: 60000});
+    await page.locator('input[type="text"]').fill('keycloak-local.alcala.org');
+    await page.getByRole('button', {name: 'Next'}).click()
 
-  await page.getByLabel('First Name').fill('John')
-  await page.getByLabel('Last Name').fill('Doe')
-  await page.getByLabel('E-mail Address').fill('john.doe@gmail.com')
-  await page.getByLabel('Password', { exact: true }).fill('test123')
-  await page.getByRole('button', { name: 'Next' }).click()
-  await sleep(3000)
+    await page.getByLabel('First Name').fill('John')
+    await page.getByLabel('Last Name').fill('Doe')
+    await page.getByLabel('E-mail Address').fill('john.doe@gmail.com')
+    await page.getByLabel('Password', {exact: true}).fill('test123')
+    await page.getByRole('button', {name: 'Next'}).click()
 
-  await page.getByRole('button', { name: 'Install Control Center' }).click()
+    await page.getByRole('button', {name: 'Install Control Center'}).click()
 
-
-
+  } else {
+    log(`Skipping the setup of Control center because of missing env variable: PASS_KEY\n`)
+  }
   // ---------------------
   await context.close();
   await browser.close();
