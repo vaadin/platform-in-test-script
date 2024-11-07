68033be71ebeacbfc0e8241e597e36092e95cc55
maximerauturier <maxime@vaadin.com>
2024-11-07 14:55:49 +0200

    Add a script to deploy an application to CC

---
+++ scripts/pit/its/cc-install-apps.js
@@ -0,0 +1,55 @@
+const { chromium } = require('playwright');
+const path = require('path');
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+let headless = false, host = 'localhost', port = '8000', hub = false;
+process.argv.forEach(a => {
+    if (/^--headless/.test(a)) {
+        headless = true;
+    } else if (/^--ip=/.test(a)) {
+        ip = a.split('=')[1];
+    } else if (/^--port=/.test(a)) {
+        port = a.split('=')[1];
+    }
+});
+
+(async () => {
+    const browser = await chromium.launch({
+        headless: headless,
+        chromiumSandbox: false
+    });
+    const context = await browser.newContext({ ignoreHTTPSErrors: true });
+    const log = s => process.stderr.write(`   ${s}`);
+
+    // Open new page
+    const page = await context.newPage();
+    page.on('console', msg => console.log("> CONSOLE:", (msg.text() + ' - ' + msg.location().url).replace(/\s+/g, ' ')));
+    page.on('pageerror', err => console.log("> PAGEERROR:", ('' + err).replace(/\s+/g, ' ')));
+
+    // Go to http://${host}:${port}/
+    await page.goto(`http://${host}:${port}/`);
+
+
+    await page.getByLabel('Email').fill('john.doe@gmail.com')
+    await page.getByLabel('Password', {exact: true}).fill('test123')
+    await page.getByRole('button', {name: 'Sign In'}).click()
+
+    await page.getByRole('button', {name: 'Manage applications'}).click()
+    await page.getByRole('button', {name: 'Deploy'}).click()
+
+    await page.getByLabel('Application Name', {exact: true}).fill('App1')
+    await page.getByLabel('Image', {exact: true}).fill('k8sdemos/bakery-cc')
+    await page.getByLabel('Application URI', {exact: true}).locator('input[type="text"]').fill('app1-local.alcala.org')
+
+    await page.getByLabel('Upload').click();
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    await page.getByText('Browse').click();
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles(path.join(__dirname, 'app1-local.alcala.org.pem'));
+
+    fileChooserPromise.then(await page.locator('.detail-layout').getByRole('button', {name: 'Deploy'}).click())
+
+    // ---------------------
+    await context.close();
+    await browser.close();
+})();
