cdaee2e0fec3dd6033bb71fc30e25632c0827364
maximerauturier <maxime@vaadin.com>
2024-11-08 14:55:34 +0200

    Refactor script and add missing version for the image

--- scripts/pit/its/cc-setup.js
+++ scripts/pit/its/cc-setup.js
@@ -1,33 +1,37 @@
-const { chromium } = require('playwright');
+const {chromium} = require('playwright');
+const log = s => process.stderr.write(`   ${s}`);
 
 let headless = false, host = 'localhost', port = '8000', hub = false;
 process.argv.forEach(a => {
-  if (/^--headless/.test(a)) {
-    headless = true;
-  } else if (/^--ip=/.test(a)) {
-    ip = a.split('=')[1];
-  } else if (/^--port=/.test(a)) {
-    port = a.split('=')[1];
-  }
+    if (/^--headless/.test(a)) {
+        headless = true;
+    } else if (/^--ip=/.test(a)) {
+        ip = a.split('=')[1];
+    } else if (/^--port=/.test(a)) {
+        port = a.split('=')[1];
+    }
 });
 
+if (!process.env.PASS_KEY) {
+    log(`Skipping the setup of Control center because of missing env variable: PASS_KEY\n`)
+    return;
+}
+
 (async () => {
-  const browser = await chromium.launch({
-    headless: headless,
-    chromiumSandbox: false
-  });
-  const context = await browser.newContext();
-  const log = s => process.stderr.write(`   ${s}`);
+    const browser = await chromium.launch({
+        headless: headless,
+        chromiumSandbox: false
+    });
+    const context = await browser.newContext();
 
-  // Open new page
-  const page = await context.newPage();
-  page.on('console', msg => console.log("> CONSOLE:", (msg.text() + ' - ' + msg.location().url).replace(/\s+/g, ' ')));
-  page.on('pageerror', err => console.log("> PAGEERROR:", ('' + err).replace(/\s+/g, ' ')));
+    // Open new page
+    const page = await context.newPage();
+    page.on('console', msg => console.log("> CONSOLE:", (msg.text() + ' - ' + msg.location().url).replace(/\s+/g, ' ')));
+    page.on('pageerror', err => console.log("> PAGEERROR:", ('' + err).replace(/\s+/g, ' ')));
 
-  // Go to http://${host}:${port}/
-  await page.goto(`http://${host}:${port}/`);
+    // Go to http://${host}:${port}/
+    await page.goto(`http://${host}:${port}/`);
 
-  if(process.env.PASS_KEY) {
     // Enter the passKey
     await page.getByLabel('Passkey').fill(process.env.PASS_KEY)
     await page.getByRole('button', {name: 'Next'}).click()
@@ -42,11 +46,7 @@ process.argv.forEach(a => {
     await page.getByRole('button', {name: 'Next'}).click()
 
     await page.getByRole('button', {name: 'Install Control Center'}).click()
-
-  } else {
-    log(`Skipping the setup of Control center because of missing env variable: PASS_KEY\n`)
-  }
-  // ---------------------
-  await context.close();
-  await browser.close();
+    // ---------------------
+    await context.close();
+    await browser.close();
 })();
