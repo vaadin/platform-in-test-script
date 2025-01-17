74c0fc9e93ac5eb9ef2becdee7d62bada11141b5
Manolo Carrasco <manolo@vaadin.com>
2025-01-17 08:43:38 +0100

    fix IT scripts setup and install apps

--- scripts/pit/its/cc-setup.js
+++ scripts/pit/its/cc-setup.js
@@ -6,12 +6,10 @@ const promisify = require('util').promisify;
 const log = s => process.stderr.write(`   ${s}`);
 const run = async cmd => (await promisify(exec)(cmd)).stdout;
 
-let headless = false, host = 'localhost', port = '8000', url, email, pass, newpass='Servantes';
+let headless = false, port = '8000', url, email, tmppass, pass='Servantes', ignoreHTTPSErrors = false;
 process.argv.forEach(a => {
     if (/^--headless/.test(a)) {
         headless = true;
-    } else if (/^--ip=/.test(a)) {
-        ip = a.split('=')[1];
     } else if (/^--port=/.test(a)) {
         port = a.split('=')[1];
     } else if (/^--url=/.test(a)) {
@@ -20,8 +18,10 @@ process.argv.forEach(a => {
         email = a.split('=')[1];
     } else if (/^--pass=/.test(a)) {
         pass = a.split('=')[1];
-    } else if (/^--newpass=/.test(a)) {
-        newpass = a.split('=')[1];
+    } else if (/^--tmppass=/.test(a)) {
+        tmppass = a.split('=')[1];
+    } else if (/^--notls/.test(a)) {
+        ignoreHTTPSErrors = true;
     } 
 });
 
@@ -35,15 +35,15 @@ if (!email) {
 }
 
 (async () => {
-    if (!pass) {
-        pass = await run(`kubectl -n control-center get secret control-center-user -o go-template="{{ .data.password | base64decode | println }}"`);
+    if (!tmppass) {
+        tmppass = await run(`kubectl -n control-center get secret control-center-user -o go-template="{{ .data.password | base64decode | println }}"`);
     }
     const browser = await chromium.launch({
         headless: headless,
         chromiumSandbox: false,
-        slowMo: 50
+        slowMo: 500
     });
-    const context = await browser.newContext();
+    const context = await browser.newContext({ ignoreHTTPSErrors: ignoreHTTPSErrors });
 
     const page = await context.newPage();
     page.on('console', msg => console.log("> CONSOLE:", (msg.text() + ' - ' + msg.location().url).replace(/\s+/g, ' ')));
@@ -54,16 +54,16 @@ if (!email) {
     await expect(page.getByLabel('Email')).toBeVisible();
 
     await page.getByLabel('Email').fill(email);
-    await page.getByLabel('Password').fill(pass);
+    await page.getByLabel('Password').fill(tmppass);
     await page.waitForTimeout(500);
     await page.getByRole('button', {name: 'Sign In'}).click()
 
-    console.log(email, pass, newpass)
+    console.log(email, tmppass, pass)
 
     await page.waitForTimeout(1000);
 
-    await page.getByLabel('New Password').fill('cc-vaadin');
-    await page.getByLabel('Confirm Password').fill('cc-vaadin');
+    await page.getByLabel('New Password').fill(pass);
+    await page.getByLabel('Confirm Password').fill(pass);
     await page.waitForTimeout(500);
     await page.getByRole('button', { name: 'Submit' }).click();
 
@@ -76,6 +76,8 @@ if (!email) {
     await page.waitForTimeout(500);
 
     await expect(page.getByRole('heading', { name: 'Applications' })).toBeVisible();
+
+    console.log(`User ${email} confgigured with password ${pass}`);
   
     await context.close();
     await browser.close();
