c776928106f160092e51147a400a4f1cedf042b8
Manolo Carrasco <manolo@vaadin.com>
2025-01-17 08:43:38 +0100

    fix IT scripts setup and install apps

--- scripts/pit/its/cc-install-apps.js
+++ scripts/pit/its/cc-install-apps.js
@@ -1,39 +1,59 @@
 const { chromium } = require('playwright');
 const path = require('path');
 const { expect } = require('@playwright/test');
+const fs = require('fs');
 
 const ADMIN_EMAIL = 'john.doe@admin.com';
 const ADMIN_PASSWORD = 'adminPassword';
 
-let headless = false, host = 'localhost', port = '8000', hub = false;
+
+
+let headless = false, port = '8000', url, email, pass='Servantes', ignoreHTTPSErrors = false;
 process.argv.forEach(a => {
     if (/^--headless/.test(a)) {
         headless = true;
-    } else if (/^--ip=/.test(a)) {
-        ip = a.split('=')[1];
     } else if (/^--port=/.test(a)) {
         port = a.split('=')[1];
-    }
+    } else if (/^--url=/.test(a)) {
+        url = a.split('=')[1];
+    } else if (/^--email=/.test(a)) {
+        email = a.split('=')[1];
+    } else if (/^--pass=/.test(a)) {
+        pass = a.split('=')[1];
+    } else if (/^--notls/.test(a)) {
+        ignoreHTTPSErrors = true;
+    } 
 });
+
+if (!email) {
+    log(`Skipping the setup of Control center because of missing --email= parameter\n`)
+    return;
+}
 
 (async () => {
     const browser = await chromium.launch({
         headless: headless,
-        chromiumSandbox: false
+        chromiumSandbox: false,
+        slowMo: 500
     });
-    const context = await browser.newContext({ ignoreHTTPSErrors: true });
+    const context = await browser.newContext({ ignoreHTTPSErrors: ignoreHTTPSErrors });
+
+    fs.writeFileSync('domain.pem', `${process.env.CC_CERT.replace(/\\n/g, "\n")}\n${process.env.CC_KEY.replace(/\\n/g, "\n")}`);
 
     // Open new page
     const page = await context.newPage();
     page.on('console', msg => console.log("> CONSOLE:", (msg.text() + ' - ' + msg.location().url).replace(/\s+/g, ' ')));
     page.on('pageerror', err => console.log("> PAGEERROR:", ('' + err).replace(/\s+/g, ' ')));
 
-    // Go to http://${host}:${port}/
-    await page.goto(`http://${host}:${port}/`);
+    await page.goto(`${url}`);
 
-    await page.getByLabel('Email').fill(ADMIN_EMAIL)
-    await page.getByLabel('Password', {exact: true}).fill(ADMIN_PASSWORD)
+    await expect(page.getByLabel('Email')).toBeVisible();
+
+    await page.getByLabel('Email').fill(email);
+    await page.getByLabel('Password').fill(pass);
+    await page.waitForTimeout(500);
     await page.getByRole('button', {name: 'Sign In'}).click()
+
 
     await page.getByRole('listitem').filter({ hasText: 'Settings'}).click()
     await page.getByRole('button', {name: 'Deploy'}).click()
@@ -46,7 +66,7 @@ process.argv.forEach(a => {
     const fileChooserPromise = page.waitForEvent('filechooser');
     await page.getByText('Browse').click();
     const fileChooser = await fileChooserPromise;
-    await fileChooser.setFiles(path.join(__dirname, 'app1-local.alcala.org.pem'));
+    await fileChooser.setFiles('domain.pem');
 
     fileChooserPromise.then(await page.locator('.detail-layout').getByRole('button', {name: 'Deploy'}).click())
 
@@ -56,6 +76,7 @@ process.argv.forEach(a => {
         .filter({ hasText: 'Applications'})
         .textContent()).toEqual('Applications1');
 
+    console.log(`Application app1 deployed successfully`);
     // ---------------------
     await context.close();
     await browser.close();
