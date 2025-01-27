8dd87013dc46b5d0b8db772d3055c666ffc141f5
Manuel Carrasco Moñino <manolo@vaadin.com>
2025-01-27 15:38:03 +0200

    more stable CC tests when waiting for readyness, and better CERT handling (#12)
    * Changes where CERTS are created and add checks for them.
    Adds stability when waiting for CC to be ready when pod crashes and ingress fails
    
    * fix filename in screenshots

--- scripts/pit/lib/lib-ccenter.sh
+++ scripts/pit/lib/lib-ccenter.sh
@@ -2,20 +2,26 @@
 . `dirname $0`/lib/lib-playwright.sh
 . `dirname $0`/lib/lib-k8s-kind.sh
 
-## Configuration
+## Domain and Host Configuration
 CC_DOMAIN=local.alcala.org
 CC_CONTROL=control.$CC_DOMAIN
 CC_AUTH=auth.$CC_DOMAIN
 CC_EMAIL=admin@$CC_DOMAIN
-# Original cert secrets are: control-center-cert and control-center-keycloak-cert
+## Secret cert names
+# Original are: control-center-cert and control-center-keycloak-cert
 CC_TLS_A=cc-control-app-tls
 CC_TLS_K=cc-control-login-tls
+## Ingress names
+CC_ING_A=control-center
+CC_ING_K=control-center-keycloak-ingress
+## K8s cluster and namespace
 CC_CLUSTER=cc-cluster
 CC_NS=control-center
+
+## UI tests to run after the control-center is installed
 CC_TESTS="cc-setup.js cc-install-apps.js"
 
 checkDockerRunning() {
-
   if ! docker ps > /dev/null 2>&1; then
     err "!! Docker is not running. Please start Docker and try again. !!"
     return 1
@@ -25,7 +31,6 @@ checkDockerRunning() {
 ## Install Control Center with Helm
 installCC() {
   [ -n "$VERBOSE" ] && D=--debug || D=""
-  ## TODO: why this fails
   [ -n "$CC_KEY" -a -n "$CC_CERT" ] && args="--set app.tlsSecret=$CC_TLS_A --set keycloak.tlsSecret=$CC_TLS_K" || args=""
   runCmd "$TEST" "Installing Vaadin Control Center" \
    "time helm install control-center oci://docker.io/vaadin/control-center \
@@ -33,11 +38,10 @@ installCC() {
     --set domain=$CC_DOMAIN \
     --set user.email=$CC_EMAIL \
     --set app.host=$CC_CONTROL --set keycloak.host=$CC_AUTH $D $args"
-  ## TODO: dont wait most times it takes a long, therefore we use our own wait
-  # --wait $D"
 }
 
 ## a loop for waiting to the control-center to be ready
+# NOTE: --wait is not working properly since there are pods restarting because of performance issues
 # $1: timeout in seconds (default 900)
 waitForCC() {
   log "Waiting for Control Center to be ready"
@@ -45,11 +49,11 @@ waitForCC() {
   while true; do
       elapsed=`expr ${elapsed:-0} + 1`
       [ "$elapsed" -ge "${1:-900}" ] && log "Timeout ${1:-900} sec. exceeded." && return 1
-      H=`kubectl get pods -n $CC_NS | egrep 'control-center-[0-9abcdef]+-..... ' | awk '{print $3" "$4}'`
+      H=`kubectl get pods -n $CC_NS | egrep 'control-center-[0-9abcdef]+-..... ' | awk '{print $2" "$3" "$4}'`
       case "$H" in
         "")
           log "Control center not installed in k8s" && return 1 ;;
-        Running*)
+        1/1*Running*)
           echo "" && log "Done ($elapsed secs.) - Status: $H"
           return 0 ;;
         *)
@@ -70,41 +74,50 @@ uninstallCC() {
   runCmd "$TEST" "Removing namespace $CC_NS" kubectl delete ns $CC_NS $KD
 }
 
+checkTls() {
+  for i in `kubectl get ingresses -n $CC_NS | grep nginx | awk '{print $1}'`; do
+    log "$i"
+    H=`kubectl get ingress $i -n $CC_NS -o jsonpath='{.spec.rules[0].host}'`
+    HS=`kubectl get ingress $i -n $CC_NS -o jsonpath='{.spec.tls[*].hosts[*]}'`
+    S=`kubectl get ingress $i -n $CC_NS -o jsonpath='{.spec.tls[*].secretName}'`
+    C=`kubectl get secret $S -n $CC_NS -o go-template='{{ index .data "tls.crt" | base64decode }}' | openssl x509 -noout -issuer -subject -enddate | tr '\n' ' '`
+    log "Host: $H is in ingress $i with TLS config\n   hosts: $HS secret: $S cert: $C"
+  done
+}
+
 ## Configure secrets for the control-center and the keycloak servers
 installTls() {
   [ -z "$CC_KEY" -o -z "$CC_CERT" ] && log "No CC_KEY and CC_CERT provided, skiping TLS installation" && return 0
   [ -n "$CC_FULL" ] && CC_CERT=$CC_FULL
   [ -z "$TEST" ] && log "Installing TLS $CC_TLS for $CC_CONTROL and $CC_AUT" || cmd "## Creating TLS files from envs"
-  f1=/tmp/cc-tls.crt
-  cmd 'echo -e "$CC_CERT" > $f1'
+  log "Creating certificate file for apps in this domain: $CC_DOMAIN.pem"
+  f1=cc-tls.crt
+  f2=cc-tls.key
   echo -e "$CC_CERT" > $f1 || return 1
-  f2=/tmp/cc-tls.key
-  cmd 'echo -e "$CC_KEY" > $f2'
   echo -e "$CC_KEY" > $f2 || return 1
-  # BASE64_KEY=`echo -n "$CC_KEY" | openssl base64 -A`
-  # BASE64_CERT=`echo -n "$CC_CERT" | openssl base64 -A`
+  cat $f1 $f2 > $CC_DOMAIN.pem
 
-  kubectl get secret $CC_TLS_A -n $CC_N >/dev/null 2>&1 && kubectl delete secret $CC_TLS_A -n $CC_NS
-  kubectl get secret $CC_TLS_K -n $CC_N >/dev/null 2>&1 && kubectl delete secret $CC_TLS_K -n $CC_NS
+  # remove old secrets if they exist (only needed for testing purposes since secrets are deleted before running the helm chart)
+  kubectl get secret $CC_TLS_A -n $CC_NS >/dev/null 2>&1 && kubectl delete secret $CC_TLS_A -n $CC_NS
+  kubectl get secret $CC_TLS_K -n $CC_NS >/dev/null 2>&1 && kubectl delete secret $CC_TLS_K -n $CC_NS
 
+  # generate new secrets
   runCmd "$TEST" "Creating TLS secret $CC_TLS_A in cluster" \
     "kubectl -n $CC_NS create secret tls $CC_TLS_A --key '$f2' --cert '$f1'" || return 1
   runCmd "$TEST" "Creating TLS secret $CC_TLS_K in cluster" \
     "kubectl -n $CC_NS create secret tls $CC_TLS_K --key '$f2' --cert '$f1'" || return 1
+  # not needed anymore
+  rm -f $f1 $f2
 
-  runCmd "$TEST" "patching $CC_TLS_A" kubectl patch ingress control-center -n $CC_NS --type=merge --patch \
+  # patch the ingress with the new secrets (only needed for testing purposes since secrets are set with the helm chart args)
+  runCmd "$TEST" "patching $CC_TLS_A" kubectl patch ingress $CC_ING_A -n $CC_NS --type=merge --patch \
     "'"'{"spec": {"tls": [{"hosts": ["'$CC_CONTROL'"],"secretName": "'$CC_TLS_A'"}]}}'"'"
-
-  runCmd "$TEST" "patching $CC_TLS_AK" kubectl patch ingress control-center -n $CC_NS --type=merge --patch \
+  runCmd "$TEST" "patching $CC_TLS_K" kubectl patch ingress $CC_ING_K -n $CC_NS --type=merge --patch \
     "'"'{"spec": {"tls": [{"hosts": ["'$CC_AUTH'"],"secretName": "'$CC_TLS_K'"}]}}'"'"
 
-  cat $f1 $f2 > /tmp/$CC_DOMAIN.pem
-  rm -f $f1 $f2
   pod=`kubectl -n $CC_NS get pods | grep control-center-ingress-nginx-controller | awk '{print $1}'` || return 1
   [ -n "$pod" ] && runCmd "$TEST" "Reloading nginx in $pod" "kubectl exec $pod -n "$CC_NS" -- nginx -s reload" || return 1
-  runCmd "$TEST" "Waiting for reloading ingress" sleep 10
-  # runCmd "$TEST" "Restaring ingress" \
-  #   "kubectl -n $CC_NS rollout restart deployment control-center-ingress-nginx-controller" || return 1
+  runCmd "$TEST" "Waiting for reloading ingress" sleep 5
 }
 
 ## Show temporary user-email and password in the terminal
@@ -120,16 +133,8 @@ showTemporaryPassword() {
 runPwTests() {
   computeNpm
   [ -n "$SKIPPW" ] && return 0
-  NO_TLS=--notls
+  [ -z "$CC_CERT" -o -z "$CC_KEY" ] && NO_TLS=--notls || NO_TLS=""
   for f in $CC_TESTS; do
-    ## loop until we get a valid https response from the control-center and keycloak
-    waitUntilHttpResponse https://$CC_CONTROL '^< HTTP.* 401' || return 1
-    waitUntilHttpResponse https://$CC_AUTH Keycloak || return 1
-    if [ -n "$CC_CERT" -a -n "$CC_KEY" ];then
-      waitUntilHttpResponse https://$CC_CONTROL 'SSL certificate verify ok|subject: CN=.*'$CC_DOMAIN || return 1
-      waitUntilHttpResponse https://$CC_AUTH 'SSL certificate verify ok|subject: CN=.*'$CC_DOMAIN || return 1
-      NO_TLS=""
-    fi
     runPlaywrightTests "$PIT_SCR_FOLDER/its/$f" "" "prod" "control-center" --url=https://$CC_CONTROL  --email=$CC_EMAIL $NO_TLS || return 1
     sleep 3
   done
@@ -154,14 +159,14 @@ runControlCenter() {
   ## Show temporary user-email and password in the terminal
   showTemporaryPassword
   ## Install TLS certificates for the control-center and keycloak
-  installTls
+  installTls && checkTls || return 1
   ## Forward the ingress (it needs root access since it uses port 443)
   forwardIngress $CC_NS || return 1
   ## Run Playwright tests
   runPwTests || return 1
   if [ -z "$KEEPCC" ]; then
     stopForwardIngress || return 1
-    deleteCluster || return 1
+    deleteCluster $CC_CLUSTER || return 1
   fi
 }
 
