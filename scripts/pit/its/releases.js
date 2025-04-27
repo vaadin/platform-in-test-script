423680af80b1caff3542e40d69c20d8e08166889
Manolo Carrasco <manolo@vaadin.com>
2025-04-27 11:20:38 +0200

    click on dismiss button

--- scripts/pit/its/releases.js
+++ scripts/pit/its/releases.js
@@ -17,7 +17,8 @@ const {log, args, createPage, closePage, takeScreenshot, waitForServerReady} = r
     await takeScreenshot(page, __filename, 'view3-loaded');
     await expect(page.getByText('Releases per version').first()).toBeVisible();
 
-    if (arg.version && arg.version !== 'current') {
+
+    if (arg.version !== 'current' ) {
         const [major, minor] = arg.version.split('.');
         const labelRegex = new RegExp(`${major}\\.${minor}, `);
         await page.getByLabel(labelRegex).click();
@@ -25,6 +26,15 @@ const {log, args, createPage, closePage, takeScreenshot, waitForServerReady} = r
         await takeScreenshot(page, __filename, `element-${labelRegex}-clicked`);
         await expect(page.getByLabel('Interactive chart').locator('text').getByText(arg.version)).toBeVisible();
         await takeScreenshot(page, __filename, `interactive-chart-${arg.version}-loaded`);
+
+        if (arg.mode == 'dev') {
+            try {
+                await page.getByText('Dismiss').click({timeout: 500});
+                await takeScreenshot(page, __filename, `dev-mode-indicator-closed`);
+            } catch (e) {
+                log('No dev mode indicator');
+            }
+        }
 
         await page.getByText(arg.version).first().click();
         await expect(page.getByRole('heading', { name: `Release Notes for ${arg.version}` })).toBeVisible();
