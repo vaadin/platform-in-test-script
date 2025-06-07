d33e5483eae73ed64af8cce9336fe01157834922
Manolo Carrasco <manolo@vaadin.com>
2025-06-07 10:00:56 +0200

    take screenshots in noop.js

--- scripts/pit/its/noop.js
+++ scripts/pit/its/noop.js
@@ -1,35 +1,18 @@
-const { chromium } = require('playwright');
 
-let headless = false, host = 'localhost', port = '8080', mode = false;
-process.argv.forEach(a => {
-  if (/^--headless/.test(a)) {
-    headless = true;
-  } else if (/^--port=/.test(a)) {
-    port = a.split('=')[1];
-  } else if (/^--mode=/.test(a)) {
-    mode = a.split('=')[1];
-  }
-});
+const {log, dismissDevmode, args, createPage, closePage, takeScreenshot, waitForServerReady} = require('./test-utils');
 
 (async () => {
-  const browser = await chromium.launch({
-    headless: headless,
-    chromiumSandbox: false
-  });
-  const sleep = ms => new Promise(r => setTimeout(r, ms));
-
-  const context = await browser.newContext();
-
-  const page = await context.newPage();
-  page.on('console', msg => console.log("> CONSOLE:", (msg.text() + ' - ' + msg.location().url).replace(/\s+/g, ' ')));
-  page.on('pageerror', err => console.log("> PAGEERROR:", ('' + err).replace(/\s+/g, ' ')));
-
-  await page.goto(`http://${host}:${port}/`);
+  const arg = args();
+  const page = await createPage(arg.headless, arg.ignoreHTTPSErrors);
+  await waitForServerReady(page, arg.url);
 
   await page.waitForSelector('#outlet > * > *:not(style):not(script)');
+  await takeScreenshot(page, __filename, 'view-loaded');
+  if (arg.mode == 'dev') {
+    dismissDevmode(page);
+    await takeScreenshot(page, __filename, 'dismissed-dev');
+  }
   const txt = await page.locator('#outlet').first().innerHTML();
-  console.log('\n====== PAGE CONTENT ======\n', txt, '\n====== END ======\n');
-
-  await context.close();
-  await browser.close();
+  log(txt);
+  await closePage(page);
 })();
