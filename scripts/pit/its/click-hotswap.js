62bd243542bd2d100cdb6543f1f614bc7ab1e2e3
Manolo Carrasco <manolo@vaadin.com>
2025-11-19 11:49:10 +0100

    simplify scripts/pit/its/click-hotswap.js

--- scripts/pit/its/click-hotswap.js
+++ scripts/pit/its/click-hotswap.js
@@ -1,23 +1,9 @@
-const { log, args, createPage, closePage, takeScreenshot, waitForServerReady, execCommand } = require('./test-utils');
-
-const compileMvn = async () => await execCommand(`${/^win/.test(process.platform) ? 'mvn.cmd' : 'mvn'} compiler:compile`);
-
-async function compile(page, name) {
-  await compileMvn();
-  log('Sleeping 10secs');
-  await page.waitForTimeout(10000);
-
-  if (/jetty/.test(name)) {
-    log('Reloading Page');
-    await page.reload();
-    await page.waitForLoadState();
-  }
-}
+const { log, args, createPage, closePage, takeScreenshot, waitForServerReady, execCommand, compileAndReload } = require('./test-utils');
 
 (async () => {
     const arg = args();
 
-    const page = await createPage(arg.headless);
+    const page = await createPage(arg.headless, arg.ignoreHTTPSErrors);
 
     await waitForServerReady(page, arg.url);
     await takeScreenshot(page, __filename, 'page-loaded');
@@ -33,7 +19,7 @@ async function compile(page, name) {
         log(`Changing ${java} and Compiling ...`);
 
         await execCommand(`perl -pi -e s/Click/Foo/g ${java}`);
-        await compile(page, arg.name);
+        await compileAndReload(page, arg.url, { name: arg.name });
         await takeScreenshot(page, __filename, 'after-compile-foo');
 
         await page.locator('text=Foo me').click({timeout:90000});
@@ -44,7 +30,7 @@ async function compile(page, name) {
         log(`Restoring ${java} and Compiling ...`);
 
         await execCommand(`git checkout ${java}`)
-        await compile(page, arg.name);
+        await compileAndReload(page, arg.url, { name: arg.name });
         await takeScreenshot(page, __filename, 'after-restore-compile');
 
         await page.locator('text=Click me').click({timeout:90000});
