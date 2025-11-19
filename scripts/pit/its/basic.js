03bf0c76f221acf966f44fb299075c4913062ae4
Manolo Carrasco <manolo@vaadin.com>
2025-11-19 12:30:22 +0100

    migrate scripts/pit/its/basic.js to use test-utils

--- scripts/pit/its/basic.js
+++ scripts/pit/its/basic.js
@@ -1,32 +1,21 @@
-const { chromium } = require('playwright');
-require('./base.js');
-
-let headless = false, host = 'localhost', port = '8080', hub = false;
-process.argv.forEach(a => {
-  if (/^--headless/.test(a)) {
-    headless = true;
-  } else if (/^--ip=/.test(a)) {
-    ip = a.split('=')[1];
-  } else if (/^--port=/.test(a)) {
-    port = a.split('=')[1];
-  }
-});
+const { log, args, createPage, closePage, takeScreenshot, waitForServerReady, dismissDevmode } = require('./test-utils');
 
 (async () => {
-  const browser = await chromium.launch({
-    headless: headless,
-    chromiumSandbox: false
-  });
-  const context = await browser.newContext();
+    const arg = args();
 
-  const page = await context.newPage();
-  page.on('console', msg => console.log("> CONSOLE:", (msg.text() + ' - ' + msg.location().url).replace(/\s+/g, ' ')));
-  page.on('pageerror', err => console.log("> PAGEERROR:", ('' + err).replace(/\s+/g, ' ')));
+    const page = await createPage(arg.headless);
 
-  await page.goto(`http://${host}:${port}/`);
-  await page.locator('text=Empty (Java) >> slot').nth(1).click();
-  await page.goto(`http://${host}:${port}/empty-view`);
+    await waitForServerReady(page, arg.url);
 
-  await context.close();
-  await browser.close();
+    // Dismiss dev mode notification if present
+    await dismissDevmode(page);
+    await takeScreenshot(page, __filename, 'page-loaded');
+
+    log('Testing Empty (Java) view navigation');
+    await page.locator('text=Empty (Java) >> slot').nth(1).click();
+    await page.goto(`${arg.url}/empty-view`);
+    await takeScreenshot(page, __filename, 'empty-view-loaded');
+
+    log('Basic test completed successfully');
+    await closePage(page);
 })();
