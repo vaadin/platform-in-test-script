7fb2b151173a198cfba65d8ddab9513b82524727
Manolo Carrasco <manolo@vaadin.com>
2025-11-19 12:17:31 +0100

    migrate scripts/pit/its/latest-java.js to use test-utils

--- scripts/pit/its/latest-java.js
+++ scripts/pit/its/latest-java.js
@@ -1,97 +1,102 @@
-const { chromium } = require('playwright');
-
-let headless = false, host = 'localhost', port = '8080', hub = false;
-process.argv.forEach(a => {
-  if (/^--headless/.test(a)) {
-    headless = true;
-  } else if (/^--ip=/.test(a)) {
-    ip = a.split('=')[1];
-  } else if (/^--port=/.test(a)) {
-    port = a.split('=')[1];
-  }
-});
+const { log, args, createPage, closePage, takeScreenshot, waitForServerReady, dismissDevmode } = require('./test-utils');
 
 (async () => {
-  const browser = await chromium.launch({
-    headless: headless,
-    chromiumSandbox: false
-  });
-  const context = await browser.newContext();
+    const arg = args();
 
-  // Open new page
-  const page = await context.newPage();
-  page.on('console', msg => console.log("> CONSOLE:", (msg.text() + ' - ' + msg.location().url).replace(/\s+/g, ' ')));
-  page.on('pageerror', err => console.log("> PAGEERROR:", ('' + err).replace(/\s+/g, ' ')));
+    const page = await createPage(arg.headless);
 
-  // Go to http://${host}:${port}/
-  await page.goto(`http://${host}:${port}/`);
+    await waitForServerReady(page, arg.url);
 
-  // Click text=Empty (Java) >> slot >> nth=1
-  await page.locator('text=Empty (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/empty-view`);
-  // Click text=Hello World (Java) >> slot >> nth=1
-  await page.locator('text=Hello World (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/hello-world-view`);
+    // Dismiss dev mode notification if present
+    await dismissDevmode(page);
+    await takeScreenshot(page, __filename, 'page-loaded');
 
-  // Fill input[type="text"]
-  await page.locator('input[type="text"]').fill('Greet');
-  // Click text=Say hello
-  await page.locator('text=Say hello').click();
-  await page.locator('text=Hello Greet');
+    log('Testing Empty (Java) view');
+    // Click text=Empty (Java) >> slot >> nth=1
+    await page.locator('text=Empty (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/empty-view`);
+    
+    log('Testing Hello World (Java) view');
+    // Click text=Hello World (Java) >> slot >> nth=1
+    await page.locator('text=Hello World (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/hello-world-view`);
 
-  // Click text=Dashboard (Java) >> slot >> nth=1
-  await page.locator('text=Dashboard (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/dashboard-view`);
-  // Click text=Card List (Java) >> slot >> nth=1
-  await page.locator('text=Card List (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/card-list-view`);
-  // Click vcf-nav-item:nth-child(5) > a > slot:nth-child(2)
-  await page.locator('vcf-nav-item:nth-child(5) > a > slot:nth-child(2)').click();
-  await page.waitForURL(`http://${host}:${port}/list-view`);
-  // Click vcf-nav-item:nth-child(6) > a > slot:nth-child(2)
-  await page.locator('vcf-nav-item:nth-child(6) > a > slot:nth-child(2)').click();
-  await page.waitForURL(`http://${host}:${port}/master-detail-view`);
-  // Click vcf-nav-item:nth-child(7) > a > slot:nth-child(2)
-  await page.locator('vcf-nav-item:nth-child(7) > a > slot:nth-child(2)').click();
-  await page.waitForURL(`http://${host}:${port}/master-detail-view-sampleaddress`);
-  // Click vcf-nav-item:nth-child(8) > a > slot:nth-child(2)
-  await page.locator('vcf-nav-item:nth-child(8) > a > slot:nth-child(2)').click();
-  await page.waitForURL(`http://${host}:${port}/master-detail-view-samplebook`);
-  // Click text=Collaborative Master-Detail (Java) >> slot >> nth=1
-  await page.locator('text=Collaborative Master-Detail (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/collaborative-master-detail-view`);
-  // Click text=Collaborative Master-Detail SampleAddress (Java) >> slot >> nth=1
-  await page.locator('text=Collaborative Master-Detail SampleAddress (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/collaborative-master-detail-view-sampleaddress`);
-  // Click text=Collaborative Master-Detail SampleBook (Java) >> slot >> nth=1
-  await page.locator('text=Collaborative Master-Detail SampleBook (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/collaborative-master-detail-view-samplebook`);
-  // Click text=Person Form (Java) >> slot >> nth=1
-  await page.locator('text=Person Form (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/person-form-view`);
-  // Click text=Address Form (Java) >> slot >> nth=1
-  await page.locator('text=Address Form (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/address-form-view`);
-  // Click text=Credit Card Form (Java) >> slot >> nth=1
-  await page.locator('text=Credit Card Form (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/credit-card-form-view`);
-  // Click text=Map (Java) >> slot >> nth=1
-  await page.locator('text=Map (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/map-view`);
-  // Click text=Chat (Java) >> slot >> nth=1
-  await page.locator('text=Chat (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/chat-view`);
-  // Click text=Rich Text Editor (Java) >> slot >> nth=1
-  await page.locator('text=Rich Text Editor (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/editor-view`);
-  // Click text=Image List (Java) >> slot >> nth=1
-  await page.locator('text=Image List (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/image-list-view`);
-  // Click text=Checkout Form (Java) >> slot >> nth=1
-  await page.locator('text=Checkout Form (Java) >> slot').nth(1).click();
-  await page.waitForURL(`http://${host}:${port}/checkout-form-view`);
+    // Fill input[type="text"]
+    await page.locator('input[type="text"]').fill('Greet');
+    // Click text=Say hello
+    await page.locator('text=Say hello').click();
+    await page.locator('text=Hello Greet');
+    await takeScreenshot(page, __filename, 'hello-world-tested');
 
-  // ---------------------
-  await context.close();
-  await browser.close();
+    log('Testing Dashboard (Java) view');
+    // Click text=Dashboard (Java) >> slot >> nth=1
+    await page.locator('text=Dashboard (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/dashboard-view`);
+    await takeScreenshot(page, __filename, 'dashboard-loaded');
+    
+    log('Testing Card List (Java) view');
+    // Click text=Card List (Java) >> slot >> nth=1
+    await page.locator('text=Card List (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/card-list-view`);
+    
+    log('Testing List view');
+    // Click vcf-nav-item:nth-child(5) > a > slot:nth-child(2)
+    await page.locator('vcf-nav-item:nth-child(5) > a > slot:nth-child(2)').click();
+    await page.waitForURL(`${arg.url}/list-view`);
+    
+    log('Testing Master-Detail views');
+    // Click vcf-nav-item:nth-child(6) > a > slot:nth-child(2)
+    await page.locator('vcf-nav-item:nth-child(6) > a > slot:nth-child(2)').click();
+    await page.waitForURL(`${arg.url}/master-detail-view`);
+    // Click vcf-nav-item:nth-child(7) > a > slot:nth-child(2)
+    await page.locator('vcf-nav-item:nth-child(7) > a > slot:nth-child(2)').click();
+    await page.waitForURL(`${arg.url}/master-detail-view-sampleaddress`);
+    // Click vcf-nav-item:nth-child(8) > a > slot:nth-child(2)
+    await page.locator('vcf-nav-item:nth-child(8) > a > slot:nth-child(2)').click();
+    await page.waitForURL(`${arg.url}/master-detail-view-samplebook`);
+    await takeScreenshot(page, __filename, 'master-detail-views-tested');
+
+    log('Testing Collaborative Master-Detail views');
+    // Click text=Collaborative Master-Detail (Java) >> slot >> nth=1
+    await page.locator('text=Collaborative Master-Detail (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/collaborative-master-detail-view`);
+    // Click text=Collaborative Master-Detail SampleAddress (Java) >> slot >> nth=1
+    await page.locator('text=Collaborative Master-Detail SampleAddress (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/collaborative-master-detail-view-sampleaddress`);
+    // Click text=Collaborative Master-Detail SampleBook (Java) >> slot >> nth=1
+    await page.locator('text=Collaborative Master-Detail SampleBook (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/collaborative-master-detail-view-samplebook`);
+
+    log('Testing Form views');
+    // Click text=Person Form (Java) >> slot >> nth=1
+    await page.locator('text=Person Form (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/person-form-view`);
+    // Click text=Address Form (Java) >> slot >> nth=1
+    await page.locator('text=Address Form (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/address-form-view`);
+    // Click text=Credit Card Form (Java) >> slot >> nth=1
+    await page.locator('text=Credit Card Form (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/credit-card-form-view`);
+    await takeScreenshot(page, __filename, 'forms-tested');
+
+    log('Testing additional Java views');
+    // Click text=Map (Java) >> slot >> nth=1
+    await page.locator('text=Map (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/map-view`);
+    // Click text=Chat (Java) >> slot >> nth=1
+    await page.locator('text=Chat (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/chat-view`);
+    // Click text=Rich Text Editor (Java) >> slot >> nth=1
+    await page.locator('text=Rich Text Editor (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/editor-view`);
+    // Click text=Image List (Java) >> slot >> nth=1
+    await page.locator('text=Image List (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/image-list-view`);
+    // Click text=Checkout Form (Java) >> slot >> nth=1
+    await page.locator('text=Checkout Form (Java) >> slot').nth(1).click();
+    await page.waitForURL(`${arg.url}/checkout-form-view`);
+    await takeScreenshot(page, __filename, 'all-java-views-tested');
+
+    log('All Java views tested successfully');
+    await closePage(page);
 })();
