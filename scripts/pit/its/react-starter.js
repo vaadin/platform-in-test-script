0fa0c220fd343c2e68d30a4672e3daa37050b2db
Manolo Carrasco <manolo@vaadin.com>
2025-11-19 12:30:22 +0100

    migrate scripts/pit/its/react-starter.js to use test-utils

--- scripts/pit/its/react-starter.js
+++ scripts/pit/its/react-starter.js
@@ -1,38 +1,28 @@
-const { chromium } = require('playwright');
-
-let headless = false, host = 'localhost', port = '8080', hub = false;
-process.argv.forEach(a => {
-  if (/^--headless/.test(a)) {
-    headless = true;
-  } else if (/^--ip=/.test(a)) {
-    ip = a.split('=')[1];
-  } else if (/^--port=/.test(a)) {
-    port = a.split('=')[1];
-  }
-});
+const { log, args, createPage, closePage, takeScreenshot, waitForServerReady, dismissDevmode } = require('./test-utils');
 
 (async () => {
-  const browser = await chromium.launch({
-    headless: headless,
-    chromiumSandbox: false
-  });
-  const context = await browser.newContext();
+    const arg = args();
 
-  const page = await context.newPage();
-  page.on('console', msg => console.log("> CONSOLE:", (msg.text() + ' - ' + msg.location().url).replace(/\s+/g, ' ')));
-  page.on('pageerror', err => console.log("> PAGEERROR:", ('' + err).replace(/\s+/g, ' ')));
+    const page = await createPage(arg.headless);
 
-  await page.goto(`http://${host}:${port}/`);
+    await waitForServerReady(page, arg.url);
 
-  await page.locator('text=Hello').nth(0).click();
-  await page.locator('input[type="text"]').fill('Greet');
-  await page.locator('text=Say hello').click();
-  await page.locator('text=Hello Greet');
+    // Dismiss dev mode notification if present
+    await dismissDevmode(page);
+    await takeScreenshot(page, __filename, 'page-loaded');
 
-  await page.locator('text=About').nth(0).click();
-  await page.locator('text=/This place/');
+    log('Testing Hello functionality');
+    await page.locator('text=Hello').nth(0).click();
+    await page.locator('input[type="text"]').fill('Greet');
+    await page.locator('text=Say hello').click();
+    await page.locator('text=Hello Greet');
+    await takeScreenshot(page, __filename, 'hello-tested');
 
-  // ---------------------
-  await context.close();
-  await browser.close();
+    log('Testing About page');
+    await page.locator('text=About').nth(0).click();
+    await page.locator('text=/This place/');
+    await takeScreenshot(page, __filename, 'about-page-tested');
+
+    log('React starter tested successfully');
+    await closePage(page);
 })();
