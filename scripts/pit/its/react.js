54e3ec6c582a32dba589ea455f2c2520c4cdf05d
Manolo Carrasco <manolo@vaadin.com>
2025-11-19 12:30:22 +0100

    migrate scripts/pit/its/react.js to use test-utils

--- scripts/pit/its/react.js
+++ scripts/pit/its/react.js
@@ -1,32 +1,20 @@
-const { chromium } = require('playwright');
-
-let headless = false, host = 'localhost', port = '8080', hub = false;
-process.argv.forEach(a => {
-  if (/^--headless/.test(a)) {
-    headless = true;
-  } else if (/^--ip=/.test(a)) {
-    ip = a.split('=')[1];
-  } else if (/^--port=/.test(a)) {
-    port = a.split('=')[1];
-  }
-});
+const { log, args, createPage, closePage, takeScreenshot, waitForServerReady, dismissDevmode } = require('./test-utils');
 
 (async () => {
-  const browser = await chromium.launch({
-    headless: headless,
-    chromiumSandbox: false
-  });
-  const context = await browser.newContext();
+    const arg = args();
 
-  const page = await context.newPage();
-  page.on('console', msg => console.log("> CONSOLE:", (msg.text() + ' - ' + msg.location().url).replace(/\s+/g, ' ')));
-  page.on('pageerror', err => console.log("> PAGEERROR:", ('' + err).replace(/\s+/g, ' ')));
+    const page = await createPage(arg.headless);
 
-  await page.goto(`http://${host}:${port}/`);
+    await waitForServerReady(page, arg.url);
 
-  await page.locator('text=Todo').nth(0).click();
+    // Dismiss dev mode notification if present
+    await dismissDevmode(page);
+    await takeScreenshot(page, __filename, 'page-loaded');
 
-  // ---------------------
-  await context.close();
-  await browser.close();
+    log('Testing React Todo view');
+    await page.locator('text=Todo').nth(0).click();
+    await takeScreenshot(page, __filename, 'todo-clicked');
+
+    log('React view tested successfully');
+    await closePage(page);
 })();
