cb48d3bd9a3b6d8341a432aff936a9913f895b3a
Manolo Carrasco <manolo@vaadin.com>
2025-11-19 12:30:22 +0100

    migrate scripts/pit/its/mpr-demo.js to use test-utils

--- scripts/pit/its/mpr-demo.js
+++ scripts/pit/its/mpr-demo.js
@@ -1,65 +1,73 @@
 const { chromium } = require('playwright');
-
-let headless = false, host = 'localhost', port = '8080', hub = false;
-process.argv.forEach(a => {
-  if (/^--headless/.test(a)) {
-    headless = true;
-  } else if (/^--ip=/.test(a)) {
-    ip = a.split('=')[1];
-  } else if (/^--port=/.test(a)) {
-    port = a.split('=')[1];
-  }
-});
+const { log, args, closePage, takeScreenshot, waitForServerReady, dismissDevmode } = require('./test-utils');
 
 (async () => {
-  const browser = await chromium.launch({
-    headless: headless
-  });
-  const context = await browser.newContext({
-    extraHTTPHeaders: {
-      'X-AppUpdate': 'FOO'
-    }
-  });
-  const page = await context.newPage();
-  page.on('console', msg => console.log("> CONSOLE:", (msg.text() + ' - ' + msg.location().url).replace(/\s+/g, ' ')));
-  page.on('pageerror', err => console.log("> PAGEERROR:", ('' + err).replace(/\s+/g, ' ')));
+    const arg = args();
 
-  await page.goto(`http://${host}:${port}`);
-  await page.evaluate(() => {
-    window.localStorage.setItem("vaadin.live-reload.dismissedNotifications","liveReloadUnavailable,preserveOnRefreshWarning")
-    window.location.reload();
-  });
+    log('Creating browser with special headers for MPR demo');
+    const browser = await chromium.launch({
+        headless: arg.headless
+    });
+    const context = await browser.newContext({
+        extraHTTPHeaders: {
+            'X-AppUpdate': 'FOO'
+        }
+    });
+    const page = await context.newPage();
+    page.on('console', msg => console.log("> CONSOLE:", (msg.text() + ' - ' + msg.location().url).replace(/\s+/g, ' ')));
+    page.on('pageerror', err => console.log("> PAGEERROR:", ('' + err).replace(/\s+/g, ' ')));
 
-  await page.locator('vaadin-notification-card[role="alert"]:has-text("Hello") div').nth(1).click();
+    await waitForServerReady(page, arg.url);
+    await takeScreenshot(page, __filename, 'page-loaded');
 
-  await page.getByRole('link', { name: 'Spreadsheet' }).click();
-  await page.waitForURL(`http://${host}:${port}/spreadsheet`);
+    await page.evaluate(() => {
+        window.localStorage.setItem("vaadin.live-reload.dismissedNotifications","liveReloadUnavailable,preserveOnRefreshWarning")
+        window.location.reload();
+    });
 
-  await page.getByText('90', { exact: true }).click();
-  await page.locator('div:nth-child(59)').dblclick();
+    await page.locator('vaadin-notification-card[role="alert"]:has-text("Hello") div').nth(1).click();
+    await takeScreenshot(page, __filename, 'notification-dismissed');
 
-  await page.locator('#cellinput').click();
-  await page.locator('#cellinput').fill('=B4*3');
-  await page.locator('#cellinput').press('Enter');  
-  await page.getByText('270').click();
+    log('Testing Spreadsheet view');
+    await page.getByRole('link', { name: 'Spreadsheet' }).click();
+    await page.waitForURL(`${arg.url}/spreadsheet`);
+    await takeScreenshot(page, __filename, 'spreadsheet-loaded');
 
-  await page.getByRole('link', { name: 'Tree' }).click();
-  await page.waitForURL(`http://${host}:${port}/tree`);
+    await page.getByText('90', { exact: true }).click();
+    await page.locator('div:nth-child(59)').dblclick();
 
+    await page.locator('#cellinput').click();
+    await page.locator('#cellinput').fill('=B4*3');
+    await page.locator('#cellinput').press('Enter');
+    await page.getByText('270').click();
+    await takeScreenshot(page, __filename, 'spreadsheet-formula-tested');
 
-  await page.locator('span.v-tree8-expander').nth(0).click()
-  await page.locator('span.v-tree8-expander').nth(1).click()
+    log('Testing Tree view');
+    await page.getByRole('link', { name: 'Tree' }).click();
+    await page.waitForURL(`${arg.url}/tree`);
+    await takeScreenshot(page, __filename, 'tree-loaded');
 
-  await page.getByRole('link', { name: 'Video' }).click();
-  await page.waitForURL(`http://${host}:${port}/video`);
-  await page.getByRole('link', { name: 'Legacy' }).click();
+    await page.locator('span.v-tree8-expander').nth(0).click()
+    await page.locator('span.v-tree8-expander').nth(1).click()
+    await takeScreenshot(page, __filename, 'tree-expanded');
 
-  await page.waitForURL(`http://${host}:${port}/legacy`);
-  await page.getByText('Here we are!').click();
+    log('Testing Video view');
+    await page.getByRole('link', { name: 'Video' }).click();
+    await page.waitForURL(`${arg.url}/video`);
+    await takeScreenshot(page, __filename, 'video-loaded');
 
-  await page.locator('vaadin-vertical-layout:has-text("SpreadsheetTreeVideoLegacy") path').click();
-  await page.waitForURL(`http://${host}:${port}`);
+    log('Testing Legacy view');
+    await page.getByRole('link', { name: 'Legacy' }).click();
+    await page.waitForURL(`${arg.url}/legacy`);
+    await page.getByText('Here we are!').click();
+    await takeScreenshot(page, __filename, 'legacy-tested');
 
-  await context.close();
-  await browser.close();
+    log('Testing navigation back to home');
+    await page.locator('vaadin-vertical-layout:has-text("SpreadsheetTreeVideoLegacy") path').click();
+    await page.waitForURL(`${arg.url}`);
+    await takeScreenshot(page, __filename, 'back-to-home');
+
+    log('MPR demo tested successfully');
+    await context.close();
+    await browser.close();
 })();
