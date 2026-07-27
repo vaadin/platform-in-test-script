9def692d5070367812dfa1ebd7978857f2d1fd25
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2026-07-27 09:18:17 +0300

    remove vaadin-quarkus workaround in 25.3

--- scripts/pit/lib/lib-patch-v25.3.sh
+++ scripts/pit/lib/lib-patch-v25.3.sh
@@ -9,14 +9,14 @@
 applyv253patches() {
   local app_=$1 type_=$2 vers_=$3
   case $app_ in
-    vaadin-quarkus)
+    ## vaadin-quarkus) (This should not be needed any more after vaadin-quarkus 3.2.1)
       ## The Quarkus vaadin-flow-codestart hardcodes the vaadin-quarkus-extension
       ## version (the latest released, e.g. 25.2.3) instead of tracking ${vaadin.version}.
       ## After bumping vaadin.version the extension is left behind, dragging in an
       ## inconsistent @vaadin/* component set that breaks the frontend build.
       ## Align it with the property so the version bump takes effect. See vaadin/quarkus#316.
-      perl -0777 -pi -e 's|(<artifactId>vaadin-quarkus-extension</artifactId>\s*<version>)[^<]+(</version>)|${1}\${vaadin.version}${2}|s' pom.xml
-      ;;
+      ## perl -0777 -pi -e 's|(<artifactId>vaadin-quarkus-extension</artifactId>\s*<version>)[^<]+(</version>)|${1}\${vaadin.version}${2}|s' pom.xml
+      ##;;
     hilla-crm-tutorial)
       ## @vaadin/react-components 25.3 added a SelectItem *component* (a value), so the
       ## former SelectItem *type* is now exported as SelectItemData. Using SelectItem in a
